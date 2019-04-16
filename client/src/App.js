b32093b1d4d779d570bfc3dc7286505361c3d30f
beastrc <makarov0114@outlook.com>
2019-04-17 00:03:29 +0200

    This closes #3. Delete unused methods.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 
 // Router
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 // Views
 import Home from "./Views/Home";
 import History from "./Views/History";
 
-import { Menu, Segment, Container, Icon, Header } from "semantic-ui-react";
+// Components
+import SiteHeader from "./Components/shared/SiteHeader";
+
+import { Menu, Segment, Container } from "semantic-ui-react";
 
 class App extends Component {
-  state = { activeItem: "" };
+  state = { activeItem: "home" };
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
@@ -20,39 +23,26 @@ class App extends Component {
     return (
       <div>
         <Router>
-          <Segment
-            inverted
-            textAlign="center"
-            style={{ padding: "1em 0em" }}
-            vertical
-          >
-            <Header as="h1" textAlign="center" icon>
-              <Icon inverted name="gem" />
-              Lottery dApp
-              <Header.Subheader style={{ color: "white" }}>
-                <pre>lose your money and make us rich</pre>
-              </Header.Subheader>
-            </Header>
-            <Menu pointing secondary inverted size="large">
-              <Container>
-                <Menu.Item
-                  name="home"
-                  active={activeItem === "home"}
-                  onClick={this.handleItemClick}
-                  as={NavLink}
-                  exact
-                  to="/"
-                />
-                <Menu.Item
-                  name="history"
-                  active={activeItem === "history"}
-                  onClick={this.handleItemClick}
-                  as={NavLink}
-                  to="/history"
-                />
-              </Container>
+          <SiteHeader />
+
+          <Container text>
+            <Menu pointing secondary>
+              <Menu.Item
+                name="home"
+                active={activeItem === "home"}
+                onClick={this.handleItemClick}
+                as={Link}
+                to="/"
+              />
+              <Menu.Item
+                name="history"
+                active={activeItem === "history"}
+                onClick={this.handleItemClick}
+                as={Link}
+                to="/history"
+              />
             </Menu>
-          </Segment>
+          </Container>
 
           <Route exact path="/" component={Home} />
           <Route path="/history" component={History} />
